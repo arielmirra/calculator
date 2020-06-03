b7a4411356f22c7cabc2af5a10b032bff0c4b355
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2020-06-02 22:51:44 -0300

    catalog POC

--- src/main/kotlin/com/calculator/model/Entities.kt
+++ src/main/kotlin/com/calculator/model/Entities.kt
@@ -3,10 +3,48 @@ package com.calculator.model
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToMany
 
 @Entity(name = "users")
 data class User(
-        @Id var id: String = "",
+        @Id @GeneratedValue var id: String = "",
         var name: String = "",
         var email: String = ""
 )
+
+@Entity(name = "catalogs")
+data class Catalog(
+        @Id @GeneratedValue var id: String = "",
+        var name: String = "",
+        var description: String = "",
+        @OneToMany var catalogElements: MutableList<Element>
+)
+
+@Entity(name = "elements")
+data class Element(
+        @Id @GeneratedValue var id: String = "",
+        var name: String = "",
+        var description: String = "",
+        var type: ElementType,
+        @OneToMany var metrics: MutableList<Metric>,
+        @OneToMany var elements: MutableList<Element>
+)
+
+enum class ElementType {
+    QualityCharacteristic,
+    QualitySubCharacteristic
+    // more more more
+}
+
+@Entity(name = "metrics")
+data class Metric(
+        @Id @GeneratedValue var id: String = "",
+        var name: String = "",
+        var standard: String = "",
+        @OneToMany var calculus: MutableList<Calculus>
+)
+
+@Entity(name = "calculus")
+abstract class Calculus {
+    @Id @GeneratedValue var id: String = ""
+}
