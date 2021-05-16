8c65722d44fb8c2dced0a89064e3fba8d7e5db30
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-05-16 10:59:20 -0300

    metric chart start

--- front/src/app/views/chart/metric-chart/metric-chart.component.ts
+++ front/src/app/views/chart/metric-chart/metric-chart.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Measurement, Metric} from '../../../models/Metric';
+import {Metric} from '../../../models/Metric';
 import {ChartDataSets} from 'chart.js';
+import {MeasurementService} from '../../../services/measurement.service';
+import {Measurement} from '../../../models/Measurement';
 
 @Component({
   selector: 'app-metric-chart',
@@ -16,27 +18,42 @@ export class MetricChartComponent implements OnInit {
   public measurementDates: string[];
   public chartDataSet: ChartDataSets;
 
-  constructor() { }
+  constructor(
+    public measurementService: MeasurementService
+  ) { }
 
   ngOnInit(): void {
     // TODO get Measurements of this.metric
-    this.measures = [
-      {_id: 1, name: 'first', value: 14, date: new Date('3/3/2021')},
-      {_id: 2, name: 'second', value: 15, date: new Date('3/4/2021')},
-      {_id: 3, name: 'third', value: 13, date: new Date('3/5/2021')},
-    ];
-    this.setValues();
-    this.setLabels();
-    this.chartDataSet = {
-      data: this.measurementValues, label: 'metric name' // TODO change to this.metric.name
-    };
+    // this.measures = [
+    //   {_id: 1, name: 'first', value: 14, date: new Date('3/3/2021')},
+    //   {_id: 2, name: 'second', value: 15, date: new Date('3/4/2021')},
+    //   {_id: 3, name: 'third', value: 13, date: new Date('3/5/2021')},
+    // ];
+    this.fetch();
+
+  }
+
+  private fetch(): void {
+    this.measurementService.getMeasurementFromMetric(this.metric.id).subscribe(list => {
+      console.log(list);
+      this.measures = list;
+      this.setValues();
+      this.setLabels();
+      this.chartDataSet = {
+        data: this.measurementValues, label: this.metric.name
+      };
+    } );
   }
 
   private setValues(): void {
     this.measurementValues = this.measures.map((m, index) => m.value);
+    console.log('MEASUREMENT VALUES');
+    console.log(this.measurementValues);
   }
 
   private setLabels(): void {
     this.measurementDates = this.measures.map((m, index) => m.date.toDateString());
+    console.log('MEASUREMENT DATES');
+    console.log(this.measurementDates);
   }
 }
